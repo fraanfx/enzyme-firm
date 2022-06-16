3ac3faf3f1a2fe3b1061973b11bdc4b927f13b55
Fran <francesc.arino@enzymeadvisinggroup.com>
2022-06-16 13:04:51 +0200

    First commit

---
+++ src/components/UISignature.tsx
@@ -0,0 +1,317 @@
+import React from "react";
+import Signature from "./firm";
+import Firm from '../components/firm'
+import '../index.css'
+import { getValue } from "@testing-library/user-event/dist/utils";
+import LogoHeader from "../img/ENZYME-Logotipo.png"
+import { Button, FormControlLabel, Switch, TextField, Typography } from "@material-ui/core";
+ 
+export interface SignatureProps {
+    fullName: string;
+    position: string;
+    mail: string;
+    phone: string;
+    calendarLink: string;
+}
+
+
+interface State extends SignatureProps {
+    withPhone: boolean;
+    withCalendar: boolean;
+    copied: boolean;
+}
+
+const initialState: State = {
+    fullName: "",
+    position: "",
+    mail: "",
+    phone: "",
+    calendarLink: "",
+    withPhone: false,
+    withCalendar: false,
+    copied: false,
+};
+
+ export function UISignature () {
+    const [state, setState] = React.useState<State>(initialState)
+    
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        if (event.target.name === "withPhone" || event.target.name === "withCalendar") {
+            setState((prevState) => ({
+                ...prevState,
+                [event.target.name]: event.target.checked,
+            }));
+        } else {
+            setState((prevState) => ({
+                ...prevState,
+                [event.target.name]: event.target.value,
+            }))
+        }
+    };
+   
+    const clearState =()=> setState(initialState);
+    const isStateChanged = () => {
+        return JSON.stringify(state) === JSON.stringify(initialState);
+    };
+const enoughData = () => {
+    let progress = 100;
+    if (state.withCalendar && state.withPhone){
+        if(
+           state.fullName &&
+           state.position &&
+           state.mail &&
+           state.phone &&
+           state.calendarLink
+        ){
+            return(
+            <Firm  
+                fullName={state.fullName} 
+                position={state.position} 
+                mail={state.mail} 
+                phone={state.phone} 
+                calendarLink={state.calendarLink}  
+            />
+
+            )
+        }else {
+            Object.entries(state).forEach(([key, value]) => {
+                if(
+                    ["fullName", "position", "mail", "phone", "calendarLink"].includes(key)
+                ) {
+                    if(getValue.length === 0){
+                        progress = progress -20;
+                    }
+                }
+            });
+        }
+    } else if (state.withCalendar){
+            if(
+               state.fullName &&
+               state.position &&
+               state.mail &&
+               state.calendarLink
+            ){
+                return(
+                    <Firm  
+                        fullName={state.fullName} 
+                        position={state.position} 
+                        mail={state.mail} 
+                        phone="902 026 289" 
+                        calendarLink={state.calendarLink}  
+                    />
+                )
+            }else {
+                Object.entries(state).forEach(([key, value]) => {
+                    if(
+                        ["fullName", "position", "mail", "calendarLink"].includes(key)
+                    ) {
+                        if(getValue.length === 0){
+                            progress = progress -25;
+                        }
+                    }
+                });
+            }
+        
+    }else if(state.withPhone){
+            if(
+               state.fullName &&
+               state.position &&
+               state.mail &&
+               state.phone
+            ){
+                return(
+                    <Firm  
+                    fullName={state.fullName} 
+                    position={state.position} 
+                    mail={state.mail} 
+                    phone={state.phone} 
+                    calendarLink="none"  
+                />
+                )
+            }else {
+                Object.entries(state).forEach(([key, value]) => {
+                    if(
+                        ["fullName", "position", "mail", "phone"].includes(key)
+                    ) {
+                        if(getValue.length === 0){
+                            progress = progress -25;
+                        }
+                    }
+                });
+            }
+            
+
+    }
+    else{
+        if(
+            state.fullName &&
+            state.position &&
+            state.mail 
+            
+         ){
+             return(
+            <Firm  
+                 fullName={state.fullName} 
+                 position={state.position} 
+                 mail={state.mail} 
+                 phone="902 026 289" 
+                 calendarLink="none"  
+             />
+             )
+         }else {
+             Object.entries(state).forEach(([key, value]) => {
+                 if(
+                     ["fullName", "position", "mail"].includes(key)
+                 ) {
+                     if(getValue.length === 0){
+                         progress = progress -33;
+                     }
+                 }
+             });
+         }
+
+    }
+}
+
+
+
+const copyToClipboard = () => {
+    let copyText = document.querySelector(".signature");
+    const range = document.createRange();
+    if (copyText) {
+        range.selectNode(copyText);
+    }
+    const windowSelection = window.getSelection();
+    if (windowSelection) {
+        windowSelection.removeAllRanges();
+        windowSelection.addRange(range);
+    }
+    try {
+        let successful = document.execCommand("copy");
+        console.log(successful ? "Success" : "Fail");
+        setState((prevState) => ({
+            ...prevState,
+            copied: true,
+        }));
+    } catch (err) {
+        console.log("Fail");
+    }
+};
+
+
+
+
+
+return(
+    <>
+        <div className="container column">
+                <div className="header">
+                    <img className="img-logo" src={LogoHeader}/>
+                    <p>Signature generator</p>
+                </div>
+
+                <div className="columns">
+                    <div className="form-column">
+                        <form action="">
+                        <input 
+                            type="text"
+                            placeholder="Tu nombre aquí"
+                            className="form--input"
+                            name="fullName"
+                            value={state.fullName}
+                            onChange={handleChange}
+                        /> 
+
+                        <input 
+                            type="text"
+                            placeholder="Tu cargo aquí"
+                            className="form--input"
+                            name="position"
+                            value={state.position}
+                            onChange={handleChange}
+                        />  
+                        
+                        <input 
+                            type="checkbox"
+                            id="isPhoneChecked"
+                            name="withPhone"
+                            checked={state.withPhone}
+                            onChange={handleChange}
+
+                        />
+                        <label>{state.withPhone ? "Movil personal" : "Movil de Enzyme"}</label>
+                      
+                        {state.withPhone &&(
+                                <input 
+                                type="text"
+                                placeholder="Tu telefono aquí"
+                                className="form--input"
+                                name="phone"
+                                value={state.phone}
+                                onChange={handleChange}
+                            />    
+                        )
+                        }
+                        <input 
+                            type="text"
+                            placeholder="Tu email aquí"
+                            className="form--input"
+                            name="mail"
+                            value={state.mail}
+                            
+                            onChange={handleChange}
+                        />  
+
+
+
+
+
+
+                        <input 
+                            type="checkbox"
+                            id="isCalendarChecked"
+                            name="withCalendar"
+                            checked={state.withCalendar}
+                            onChange={handleChange}
+                        />
+                         <label>{state.withCalendar ? "Enlace de reunion google meet" : "Sin enlace de reunión"}</label>
+                       
+                        
+                        {state.withCalendar &&(
+
+                                <input 
+                                        type="text"
+                                        placeholder="Link reunión"
+                                        className="form--input"
+                                        name="calendarLink"
+                                        value={state.calendarLink}
+                                        onChange={handleChange}
+                                />    
+                                    
+                        )
+                        
+                        } 
+                        <div className="container-buttons">
+                              
+                            <a className="button-style" onClick={copyToClipboard}>Copiar firma</a> 
+                            <a className="button-style" href="http://gmail.com/#settings/general" target="_blank" rel="noreferrer">Cambiar firma</a>                      
+                            <button className="button-style"  onClick={clearState}>Limpiar estado</button>
+                        </div> 
+                         </form>
+                    </div>
+                    <div className="signature-column">
+                        {enoughData()}
+                    </div>
+                </div>
+
+                <div className='footer-sutil'>
+          <p>Made with <div className='heart'>&#128153;</div>  from Enzyme Factory</p>
+      </div> 
+        </div>
+        
+    </>
+)
+
+
+
+ }
---
+++ src/components/firm.tsx
@@ -0,0 +1,134 @@
+import { SignatureProps } from '../components/UISignature'
+import LogoHeader from "../img/ENZYME-Logotipo.png"
+import Linkedin from '../img/Linkedin.svg'
+import Blog from '../img/Blogblog-svg1x.svg'
+import Descargable from '../img/Descargabledown-svg.svg'
+import '../App.css'
+
+const Signature = (props:SignatureProps) =>{
+    return(
+        <table cellPadding={0} cellSpacing={0} className={"signature"} style={{
+            
+            fontFamily: 'Arial',
+        }}>
+    <tbody>
+        
+    <tr>
+        <td rowSpan={1}>
+            <a href="http://enzyme.biz/" target="_blank" rel='noreferrer'>
+            <img
+                src="https://signature.enzyme.es/AF-ENZYME-Logotipo-RGB-POSITIVO.svg"
+                alt="Enzyme Logo"
+                width="154.64px"
+                height="31px"
+                style={{
+                    marginBottom: '15px'
+                }}
+            />
+            </a>
+        </td>
+    </tr>
+    <tr>
+        <td><b style={{fontFamily: 'Arial',
+        fontStyle: 'normal',
+        fontWeight: '700',
+        fontSize: '16px',
+        lineHeight: '18px',
+        color: '#2D3A49'}}>{props.fullName}</b></td>
+    </tr>
+    <tr>
+        <td rowSpan={1}><p style={{fontFamily: 'Arial',
+        fontStyle: 'normal',
+        fontWeight: '400',
+        fontSize: '16px',
+        lineHeight: '18px',
+        color: '#2D3A49'}}>{props.position}</p></td>
+    </tr>
+    <tr>
+        <td rowSpan={1}><a style={{fontFamily: 'Arial',
+        fontStyle: 'normal',
+        fontWeight: '400',
+        fontSize: '16px',
+        lineHeight: '18px',
+        color: '#2D3A49'}}>M. +34 {props.phone}
+        </a></td>
+    </tr>
+    <tr>
+        <td rowSpan={1}>{props.mail}</td>
+    </tr>
+    
+    
+            {props.calendarLink !== "none" &&
+              <td > <p> Puedes agendar una reunión conmigo en el siguiente <a href={props.calendarLink} target="_blank" rel="noreferrer" style={{fontFamily: 'Arial',
+              fontStyle: 'normal',
+              fontWeight: '700',
+              color:'#2D3A49',
+              fontSize: '16px',
+              lineHeight: '18px',
+                textDecoration: 'underline',
+            }}>link</a> </p> </td>
+            }
+             
+         
+    <tr  style={{marginTop: "20px", marginBottom: "20px"}}>
+       <td> 
+        <tr style={{marginRight: "30px",}}>
+            <tr>
+                <b style={{fontWeight:"700", fontSize:"12px", lineHeight: "14px", color:"#2D3A49" }}>Barcelona</b>
+            </tr>
+            
+            <tr>
+            <p style={{fontFamily: 'Arial', fontStyle: 'normal', fontWeight: '400', fontSize: '12px', lineHeight: '14px'}}>Passeig de Gràcia 17, 4ª planta </p>
+            </tr>
+            <tr>
+            <p style={{fontFamily: 'Arial', fontStyle: 'normal', fontWeight: '400', fontSize: '12px', lineHeight: '14px'}}>08007 Barcelona</p>
+            </tr>
+        </tr>
+   
+          </td>
+        <td>
+        <tr style={{marginLeft: '50px'}}>
+            <tr>
+                <b style={{fontFamily: 'Arial',fontWeight:"700", fontSize:"12px", lineHeight: "14px", color:"#2D3A49" }}>Madrid</b>
+            </tr>
+            <tr >
+            <p style={{fontFamily: 'Arial', fontStyle: 'normal', fontWeight: '400', fontSize: '12px', lineHeight: '14px'}}>Paseo de la Castellana 77, 6º</p>
+            </tr>
+            <tr>
+            <p style={{fontFamily: 'Arial', fontStyle: 'normal', fontWeight: '400', fontSize: '12px', lineHeight: '14px'}}>28046 Madrid </p>
+            </tr>
+        </tr>
+        </td>
+            </tr>
+    <tr>
+        <br />
+        <tr >
+            <tr>
+                <td>
+                <a style={{marginRight: "5px",}} href={"https://www.linkedin.com/company/enzymeadvisinggroup/mycompany/"} rel="noreferrer" target="_blank">
+                    <img src="https://signature.enzyme.es/Linkedin.svg" width={"22px"} height={"22px"} alt={"Linkedin"}/>
+                </a>
+                </td>
+                <td>
+                <a style={{marginRight: "5px",}} href="" rel="noreferrer" target="_blank">
+                    <img src="https://signature.enzyme.es/Blogblog-svg1x.svg" width={"22px"} height={"22px"} alt={"Blog"}/>
+                </a>
+                </td>
+                <td>
+                <a href="https://signature.enzyme.es/Descargabledown-svg.svg" target="_blank" rel="noreferrer">
+                    <img src={Descargable} width={"22px"} height={"22px"} alt={"Descargable"}/>
+                </a>
+                </td>
+                    
+               
+               
+            </tr>
+        </tr>
+    </tr>
+    
+    
+    </tbody>
+</table>
+    )
+}
+export default Signature;
